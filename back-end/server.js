ff99c9799a4ed8551d1e27d09a6cac379eff1254
Ben Carlson <benocarlson@gmail.com>
2020-04-15 10:03:03 -0600

    fix boilerplate back-end

---
+++ back-end/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+mongoose.connect('mongodb://localhost:27017/pagliaccio', {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
+
+app.listen(3000, () => console.log('Server listening on port 3000!'));
